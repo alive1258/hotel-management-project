62a063176b6350d3db9c6282b634667320cd91c9
zamirulkabir <zamirulkabir999@gmail.com>
2023-01-26 17:49:45 +0600

    first commit

--- src/common/Empty/EmptyFile.js
+++ src/common/Empty/EmptyFile.js
@@ -1,14 +1,14 @@
-import React from "react"
-import "./EmptyFile.css"
+import React from "react";
+
 
 const EmptyFile = () => {
   return (
     <>
-      <section className='error'>
-        <h1>Error 404 !</h1>
+      <section className='h-1/2'>
+        <h1 className="font-lg text-red-500">Error 404 !</h1>
       </section>
     </>
   )
 }
 
-export default EmptyFile
+export default EmptyFile;
--- src/common/HeadTitle/HeadTitle.js
+++ src/common/HeadTitle/HeadTitle.js
@@ -1,27 +1,27 @@
-import React from 'react';
-import './HeadTitle.css';
-import { useLocation ,Link} from 'react-router-dom';
+import React from "react"
+import "./HeadTitle.css"
+import { useLocation, Link } from "react-router-dom"
 
 const HeadTitle = () => {
-    const location = useLocation()
-    return (
-        <div>
-            <section className='image-heading'>
-                <div className='container'>
-                    {/* jokon je page jaby tokon se page curent location dekaby nice coad {location.pathname.split("/")[1]} */}
-                    <h1>{location.pathname.split("/")[1]}</h1>
-                  
-                    <div className='btn-h-a'>
-                        <Link to="/">Home /</Link>
-                        <span>{location.pathname.split("/")[1]}</span>
-                    </div>
+  const location = useLocation()
 
-                    
-                    
-                </div>
-            </section>
+  return (
+    <>
+      <section className='image-heading bg-center -z-10 h-1/3 relative bg-cover bg-no-repeat text-white'>
+        <div className='container flex justify-between items-center text-center'>
+          {/*<h1>{location.pathname.slice(1, -2, -1)}</h1>*/}
+          <h1 className="text-2xl capitalize">{location.pathname.split("/")[1]}</h1>
+          {/*<h1>{location.pathname.replace(/ /, "")}</h1>*/}
+
+          <button >
+            <Link className="text-[#31b675] cursor-pointer" to='/'> Home / </Link>
+            <span className="capitalize tracking-wider">{location.pathname.split("/")[1]}</span>
+            {/*<span>{location.pathname.replace("/", " ")}</span>*/}
+          </button>
         </div>
-    );
-};
+      </section>
+    </>
+  )
+}
 
-export default HeadTitle;
+export default HeadTitle
